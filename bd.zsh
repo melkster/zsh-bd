9e5a773284c4be1789d6c9e65b14820c99b27163
Melker Österberg <melkerosterberg@gmail.com>
2020-09-12 19:37:05 +0200

    Do `cd ..` on `bd` without arguments
    Go up one directory on `bd` instead of printing usage

--- bd.zsh
+++ bd.zsh
@@ -1,8 +1,7 @@
 bd () {
   (($#<1)) && {
-    print -- "usage: $0 <name-of-any-parent-directory>"
-    print -- "       $0 <number-of-folders>"
-    return 1
+    cd ..
+    return 0
   } >&2
   # example:
   #   $PWD == /home/arash/abc ==> $num_folders_we_are_in == 3
